9d06840b7f9fb621573ef39dd0af2b98d8a800ae
koossaayy <koossaayy@users.noreply.github.com>
2021-11-26 21:06:12 +0000

    Fix styling

--- src/Components/Mapbox.php
+++ src/Components/Mapbox.php
@@ -2,23 +2,20 @@
 
 namespace Koossaayy\LaravelMapbox\Components;
 
-use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
 
 class Mapbox extends Component
 {
-
     public function __construct(
         public String $id,
         public array $center = [0,0],
-        public int $zoom=8,
+        public int $zoom = 8,
         public bool $navigationControls = false,
         public string $mapStyle = 'mapbox/streets-v11',
         public bool $interactive = true,
         public array $markers = [],
-    )
-    {
-
+    ) {
     }
 
     public function render(): View
