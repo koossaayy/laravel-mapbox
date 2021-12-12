13fa264b55afebbfe0109fa8b0d4f53e95b9eb21
Koussay <tounessna.2010@gmail.com>
2021-12-13 00:03:49 +0100

    release of the version 0.2.0, check changelog for more

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,9 @@ All notable changes to `laravel-mapbox` will be documented in this file.
 ## 0.0.1 - 2021-29-11
 
 - initial release
+
+## 0.0.2 - 2021-13-12
+- Added RTL Support for the names of places 
+- Added support to cooperative gestures
+- Added free marker support, so the end user can interact with the map
+
--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,11 +1,21 @@
 <script>
     mapboxgl.accessToken = '{{ config('mapbox.mapbox_token', null) }}';
+
+    @if ($rtl ?? '')
+        mapboxgl.setRTLTextPlugin(
+        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+        null,
+        true
+        );
+    @endif
+
     const map = new mapboxgl.Map({
         container: '{{ $id }}',
         style: 'mapbox://styles/{{ $mapStyle }}',
         center: [{{ $center['long'] ?? $center[0] }}, {{ $center['lat'] ?? $center[1] }}],
         zoom: {{ $zoom }},
         interactive: {{ $interactive ? 'true' : 'false' }},
+        cooperativeGestures: {{ $cooperativeGestures ? 'true' : 'false' }},
     });
 
     {{ $navigationControls ? 'map.addControl(new mapboxgl.NavigationControl());' : '' }}
@@ -13,6 +23,17 @@
     map.on('load', function() {
         map.resize();
     });
+
+
+    @if ($draggable ?? '')
+        let long = 0;
+        let lat = 0;
+        const marker = new mapboxgl.Marker({
+        draggable: true
+        })
+        .setLngLat([0, 0])
+        .addTo(map);
+    @endif
 
     @foreach ($markers as $key => $marker)
         new mapboxgl.Marker()
