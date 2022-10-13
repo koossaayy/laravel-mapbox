dc9004b611c2dc96af3bb11913f411138113d17c
Koussay <tounessna.2010@gmail.com>
2022-10-13 12:20:29 +0100

    running tests

--- resources/views/components/mapbox.blade.php
+++ resources/views/components/mapbox.blade.php
@@ -6,9 +6,14 @@
         {{ $attributes['style'] && Str::contains($attributes['style'], 'width') ? $attributes['style'] : $attributes['style'] . 'width: 100%;' }}
     }
 
+    .marker {
+        display: block;
+        border: none;
+        cursor: pointer;
+        padding: 0;
+    }
 </style>
 
 <div id="{{ $id }}" class="{{ $attributes['class'] ?? '' }}"></div>
 
 @include('mapbox::includes.script')
-
