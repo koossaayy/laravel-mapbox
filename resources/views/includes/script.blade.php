b521189ece5f252c046ff9957d38c5b83bd6dbbc
Koussay <tounessna.2010@gmail.com>
2024-11-14 14:28:32 +0100

    fixing another javascript error

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,12 +1,10 @@
 <script>
     mapboxgl.accessToken = '{{ config('mapbox.mapbox_token', null) }}';
 
-    @if ($rtl ?? '')
+    @if ($rtl)
         mapboxgl.setRTLTextPlugin(
-            'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
-            null,
-            true
-        );
+            'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js', null,
+            true);
     @endif
 
     const map{{ $id }} = new mapboxgl.Map({
